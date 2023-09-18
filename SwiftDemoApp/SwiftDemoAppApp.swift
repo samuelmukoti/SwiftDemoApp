6b67163596fbb14e94fd802496d394fec2758c71
Samuel Mukoti <sam@melivo.com>
2023-09-18 13:09:43 -0700

    Initial Commit

---
+++ SwiftDemoApp/SwiftDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDemoAppApp.swift
+//  SwiftDemoApp
+//
+//  Created by Samuel Mukoti on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
